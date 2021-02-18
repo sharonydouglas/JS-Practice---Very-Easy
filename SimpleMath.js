eef4f3d51a9dcad873ff7b8b68d4cfd13548c5e0
sharonydouglas <78060819+sharonydouglas@users.noreply.github.com>
2021-02-18 14:47:36 -0600

    Create SimpleMath.js
    Practice completing simple math functions

---
+++ SimpleMath.js
@@ -0,0 +1,17 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+function addition(a, b) {
+	return a + b;
+	
+}
+// Write a function that takes the base and height of a triangle and return its area.
+
+function triArea(base, height) {
+return base * height;
+}
+
+// Write a function that takes an integer minutes and converts it to seconds.
+
+function convert(minutes) {
+return minutes * 60; 
+}
